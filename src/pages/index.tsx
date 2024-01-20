ff7cfc00cd53b021b67a654f71761cd22770c223
Michael Ciabuca <ciabuca@gmail.com>
2024-01-20 12:50:57 +0200

    added light/dark mode toggle utilizing shadcnui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,24 @@ import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { SelectValue, SelectTrigger, SelectItem, SelectContent, Select } from "@/components/ui/select"
 import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/ui/ModeToggle";
+
 
 const Home = () => {
   const [url, setUrl] = useState('');
   const [errorCorrectionLevel, setErrorCorrectionLevel] = useState('M');
   const [qrCodeUrl, setQrCodeUrl] = useState('');
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await generateQR();
+  };
+
   const generateQR = async () => {
     try {
       const options: QRCodeToDataURLOptions = {
-        errorCorrectionLevel: 'H' // 'L', 'M', 'Q', 'H'
+        errorCorrectionLevel: 'H',
+        scale: 10,
       };
       const qrImageUrl = await QRCode.toDataURL(url, options);
       if (typeof qrImageUrl === 'string') {
@@ -30,18 +38,23 @@ const Home = () => {
 
   return (
     <div className="grid w-full max-w-2xl gap-6 p-4 mx-auto">
-      <div className="grid gap-4">
-        <Label htmlFor="data">Data / URL</Label>
-        <Input id="data" placeholder="Enter data or URL" value={url} onChange={(e) => setUrl(e.target.value)} />
+      <div className="flex justify-end">
+        <ModeToggle />
       </div>
-      <div className="flex flex-col items-center space-y-4">
-        <Button variant="outline" onClick={generateQR}>Generate QR Code</Button>
-        <h2 className="text-lg font-semibold">QR Code Preview</h2>
-        <div className="w-64 h-64 bg-gray-100 rounded-md flex items-center justify-center">
-          {qrCodeUrl && <Image alt="QR Code" src={qrCodeUrl} width={500} height={500} className="aspect-square object-contain" />}
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div className="grid gap-4">
+          <Label htmlFor="data">Data / URL</Label>
+          <Input id="data" placeholder="Enter data or URL" value={url} onChange={(e) => setUrl(e.target.value)} />
         </div>
-        {/* add a download button here later */}
-      </div>
+        <div className="flex flex-col items-center space-y-4">
+          <Button onClick={generateQR}>Generate QR Code</Button>
+          <h2 className="text-lg font-semibold">QR Code Preview</h2>
+          <div className="w-64 h-64 bg-gray-100 rounded-md flex items-center justify-center">
+            {qrCodeUrl && <Image alt="QR Code" src={qrCodeUrl} width={500} height={500} className="aspect-square object-contain" />}
+          </div>
+          {/* add a download button here later */}
+        </div>
+      </form>
     </div>
   );
 }
